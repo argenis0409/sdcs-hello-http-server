6b4fbcc04776071bde6cd57fcc772bee9d5a4db5
argenis0409 <argenis04_09@icloud.com>
2019-05-08 14:33:24 -0700

    hello http server

---
+++ README.md
@@ -0,0 +1,4 @@
+Hello http server.
+
+in this project i had to build a simple web server that will respond to a basic request for "http://localhost:8080/."
+
--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // write your code here
+var http = require("http");
+http.createServer(function(req, res){
+res.writeHead(200);
+res.end('Hello World');
+}).listen(8080, '127.0.0.1');
+console.log('Server is Listening at http://127.0.0.1:8080/');
